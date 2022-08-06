89e221b21aab538c2e72b0b06acbb23f7a9252f1
pavani0104 <110731002+pavani0104@users.noreply.github.com>
2022-08-06 23:02:19 +0800

    Add files via upload
    Day1

---
+++ day1/CheckiftheNumberPositiveorNegitive.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class CheckiftheNumberPositiveorNegitive {
+
+
+ public static void main(String args[])
+ {
+	 int number = -56;
+	 
+	 if(number>0)
+	 {
+	System.out.println(" the given number is positive ");
+	 }
+	else
+		
+	{
+		if(number<0)
+		{
+		System.out.println("the given number is negative");
+		
+	}
+		else
+		{
+			System.out.println("the given number is neither positive nor negative");
+		}
+	 }
+ }
+}
