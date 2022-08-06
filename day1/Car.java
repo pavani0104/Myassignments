2d69ce856ad4c392149bec84b152c1faf2cbad6f
pavani0104 <110731002+pavani0104@users.noreply.github.com>
2022-08-06 22:36:59 +0800

    Add files via upload

---
+++ day1/Car.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Car {
+public byte seats = 7;
+ short regNo=2233 ;
+int newPrice  = 550000;
+long serialNo = 3574385438543L;
+float milage = 16.4F;
+double soldPrice = 45.555;
+char series = 'T';
+String name = "safari";
+boolean isInCondition = true;
+
+}
+
+	
+
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class TwoWheeler {
+short noOfMirrors=2;
+int noOfWheels=2;
+long chasisNumber=23523534L;
+boolean isPictured= true;
+String bikeName= "Apache";
+double runningKM=232.234;
+
+
+}
